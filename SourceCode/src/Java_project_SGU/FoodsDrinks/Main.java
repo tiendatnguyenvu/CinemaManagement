28aec27caa9bd3b881b131ca944555070966c4db
Nguyen Vu Tien Dat <nguyenvutiendat07022003@gmail.com>
2023-10-10 11:17:12 +0700

    update

--- SourceCode/src/Java_project_SGU/FoodsDrinks/Main.java
+++
@@ -1,8 +0,0 @@
-package Java_project_SGU.FoodsDrinks;
-public class Main {
-    public static void main(String args[]) {
-//        managerFrame screen=new managerFrame();
-        userFrame screen = new userFrame();
-        screen.setVisible(true);
-    }
-}
